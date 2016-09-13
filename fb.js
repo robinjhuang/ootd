f14453f5bbd51fbc3fe3d2654e2274eb4a3e9850
Junhan Huang <robinhuang@Junhans-MacBook-Pro.local>
2016-09-12 23:35:17 -0400

    implemented facebook login/authentication

---
+++ fb.js
@@ -0,0 +1,6 @@
+// facebook app settings - fb.js
+module.exports = {
+  'appID' : '1779099545701822',
+  'appSecret' : '4bb5e7b31c64d86f44bc2dab9e0341de',
+  'callbackUrl' : 'http://localhost:3000/login/facebook/callback'
+}
