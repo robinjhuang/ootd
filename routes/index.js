1c75cc18fc68af66c2c45b8952baca79534d7320
Junhan Huang <robinhuang@Junhans-MacBook-Pro.local>
2016-09-12 23:53:22 -0400

    implemented fb login

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,64 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var isAuthenticated = function (req, res, next) {
+	// if user is authenticated in the session, call the next() to call the next request handler 
+	// Passport adds this method to request object. A middleware is allowed to add properties to
+	// request and response objects
+	if (req.isAuthenticated())
+		return next();
+	// if the user is not authenticated then redirect him to the login page
+	res.redirect('/');
+}
 
-module.exports = router;
+module.exports = function(passport){
+
+	/* GET login page. */
+	router.get('/', function(req, res) {
+    	// Display the Login page with any flash message, if any
+    	console.log(req.user);
+		res.render('index', { message: req.flash('message'), user: req.user });
+	});
+
+	// route for facebook authentication and login
+	// different scopes while logging in
+	router.get('/login/facebook', 
+	  passport.authenticate('facebook', { scope : ['email'] }
+	));
+	 
+	// handle the callback after facebook has authenticated the user
+	router.get('/login/facebook/callback',
+	  passport.authenticate('facebook', {
+	    successRedirect : '/',
+	    failureRedirect : '/'
+	  })
+	);
+
+	/* Handle Login POST */
+	router.post('/login', passport.authenticate('login', {
+		successRedirect: '/',
+		failureRedirect: '/',
+		failureFlash : true  
+	}));
+
+	/* GET Registration Page */
+	router.get('/signup', function(req, res){
+		res.render('register',{message: req.flash('message')});
+	});
+
+	/* Handle Registration POST */
+	router.post('/signup', passport.authenticate('signup', {
+		successRedirect: '/',
+		failureRedirect: '/signup',
+		failureFlash : true  
+	}));
+
+
+	/* Handle Logout */
+	router.get('/signout', function(req, res) {
+		req.logout();
+		res.redirect('/');
+	});
+
+	return router;
+}
